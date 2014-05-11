2a89fa93fd753ef3a583bc78a28c1b59c5912466
Jakob <jakob.boss@gmail.com>
2014-05-11 23:21:44 +0200

    Admin update

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,17 +1,36 @@
 ActiveAdmin.register User do
+  # Hide filters, because they are annoying
+  config.filters = false
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
+
+  index do
+    column :id
+
+    column :img do |u|
+      image_tag(u.image_url, width: 50)
+    end
+
+    column :name, sortable: :name do |u|
+      div link_to(u.name, admin_user_path(u.id))
+      div u.email
+    end
+
+    column :gender, sortable: :gender do |u|
+      if u.gender.present?
+        image_tag(u.male? ? "male.png" : "female.png", width: 20)
+      end
+    end
+
+    column :Favorites do |u|
+      div "Favorited #{u.favorites.count} shows"
+    end
+
+    column "Created", sortable: :created_at do |u|
+      "#{time_ago_in_words u.created_at} ago"
+    end
+
+    default_actions
+  end
+
+
 end
