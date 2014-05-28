2aff203295cc513a6ad53cae68b027534ac6fb77
Jakob <jakob.boss@gmail.com>
2014-05-28 22:12:01 +0200

    Get new token from google

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@ class User < ActiveRecord::Base
 	def self.create_with_omniauth(auth)
 	  create! do |user|
 	    user.provider = auth["provider"]
+	    user.token = auth["credentials"]["token"]
 	    user.uid = auth["uid"]
 	    user.name = auth["info"]["name"]
 	    user.email = auth["info"]["email"]
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'],
+  {
+    :name => "google",
+    :scope => [ "userinfo.email",
+                "userinfo.profile",
+                "https://www.googleapis.com/auth/calendar"
+              ].join(", "),
+    :image_aspect_ratio => "square",
+    :image_size => 50
+  }
 end
