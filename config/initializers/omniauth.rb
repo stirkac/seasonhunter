00457d41b24c77453f4f0e44a012af23f82ab75e
Jakob <jakob.boss@gmail.com>
2014-05-28 22:29:51 +0200

    Fixed omniauth login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,10 @@
+require 'google/calendar'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'],
-  {
-    :name => "google",
-    :scope => [ "userinfo.email",
+  { :scope => [ "userinfo.email",
                 "userinfo.profile",
                 "https://www.googleapis.com/auth/calendar"
-              ].join(", "),
-    :image_aspect_ratio => "square",
-    :image_size => 50
+              ].join(", ")
   }
 end
